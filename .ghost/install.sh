c4b93cd97464cd01d5e69ce362c24c0a120ac33e
René Preuß <rene@preuss.io>
2021-06-18 00:26:32 +0200

    Update install.sh

--- .ghost/install.sh
+++ .ghost/install.sh
@@ -10,16 +10,21 @@ fi
 LC_ALL=C.UTF-8
 PHP_PPA="ondrej/php"
 
+mkdir -p /etc/apt/sources.list.d/
+
 if ! grep -q "^deb .*$PHP_PPA" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
     # commands to add the ppa ...
     add-apt-repository ppa:ondrej/php
 fi
 
 # install all dev tools and php
-sudo apt -y install git snapd openssl curl build-essential \
+sudo apt -y install zsh git snapd openssl curl build-essential \
 	wget gnupg2 gnupg-agent dirmngr cryptsetup scdaemon pcscd secure-delete hopenpgp-tools yubikey-personalization \
 	php-common php-curl php-json php-mbstring php-mysql php-xml php-zip php-swoole php-gd php-imagick \
 	php-redis redis-server mariadb-server flameshot
+
+# todo add zsh installer
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # "gpg --card-edit" should now work.
 
@@ -39,9 +44,11 @@ curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 rm composer-setup.php
 
+# install composer packages globally
+composer global require ghostzero/maid
+
 # install snap apps
 sudo snap install phpstorm --classic
 sudo snap install slack --classic
 sudo snap install datagrip --classic
 sudo snap install discord
-sudo snap install flameshot
